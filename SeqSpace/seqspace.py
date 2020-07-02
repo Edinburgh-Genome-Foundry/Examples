4382c00f7c83f34b6d2634f6983abbb5974404a1
Peter Vegh <pvegh@posteo.uk>
2020-07-02 21:08:27 +0100

    read_seqspace()

--- SeqSpace/seqspace.py
+++ SeqSpace/seqspace.py
@@ -30,3 +30,40 @@ class SeqSpace:
         f = open(path, "wb")
         f.write(filetext.encode("utf8"))
         f.close()
+
+
+def read_seqspace(path, separator=",", choice_separator="|"):
+    f = open(path, "r", encoding="utf8")
+    lines = f.read().splitlines()
+    f.close()
+
+    if lines[0][0] != ">":
+        raise ValueError("Invalid sequence format")
+    name = lines[0][1:]  # remove '>'
+
+    if lines[1][-1] != choice_separator:
+        raise ValueError(
+            "Invalid sequence format: sequence must end in " + choice_separator
+        )
+    sequence_list = lines[1].split(choice_separator)
+    sequence_list.pop()  # the empty string after last choice_separator
+
+    sequence_counter = 0
+    mutation_choices = []
+
+    for seq in sequence_list:
+        seqs = seq.split(separator)
+        mutationchoice_length = len(seqs[0])
+        loc = (sequence_counter, sequence_counter + mutationchoice_length)
+        choice = dnachisel.MutationSpace.MutationChoice(loc, seqs)
+
+        mutation_choices += [
+            choice
+        ] * mutationchoice_length  # one MutationChoice for each letter position
+        sequence_counter += mutationchoice_length
+
+    space = dnachisel.MutationSpace.MutationSpace(mutation_choices)
+
+    seq_space = SeqSpace(space, name, separator=",", choice_separator="|")
+
+    return seq_space
