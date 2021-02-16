ebb8afbd11723504ed1e47e8dc11fce8936808d5
Peter Vegh <pvegh@posteo.uk>
2021-02-16 19:33:43 +0000

    Flag stop codons

--- Minotaor/minotaor.py
+++ Minotaor/minotaor.py
@@ -11,6 +11,13 @@ seq_dataset = pandas.read_csv(os.path.join(script_dir, "seq.csv"))
 
 
 def annotate_record(seqrecord, seq_dataset=seq_dataset):
+    # FLAG STOP CODONS: *
+    stop_positions = [i for i, letter in enumerate(str(seqrecord.seq)) if letter == "*"]
+    for position in stop_positions:
+        seqrecord.features.append(
+            SeqFeature(FeatureLocation(position, position + 1), type="STOP")
+        )
+    # ANNOTATE SEQUENCES
     sequences = seq_dataset["sequence"].to_list()
     names = seq_dataset["name"].to_list()
     for index, sequence in enumerate(sequences):
