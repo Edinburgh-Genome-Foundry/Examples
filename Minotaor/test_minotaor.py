6e48230a38d0dee2afd4751560d7e44e7ea33119
Peter Vegh <pvegh@posteo.uk>
2021-02-16 20:13:09 +0000

    Add tests

---
+++ Minotaor/test_minotaor.py
@@ -0,0 +1,15 @@
+import minotaor
+
+
+def test_annotate_record():
+    protein = minotaor.Seq("HHHHHHDLG*EDINBURGHGENQMEFQUNDRY*")
+    protein_record = minotaor.SeqRecord(
+        protein, id="example", annotations={"molecule_type": "protein"}
+    )
+
+    protein_record = minotaor.annotate_record(protein_record)
+
+    assert len(protein_record.features) == 5
+    assert protein_record.features[0].id == "no start codon"
+    assert protein_record.features[1].id == "STOP"
+    assert protein_record.features[3].id == "6xHis"
